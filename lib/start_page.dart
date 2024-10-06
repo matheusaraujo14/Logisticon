92d8cc0014ddebabf6740dd165f9327d6bcba0c4
PedroAndradeDorighello <139513519+PedroAndradeDorighello@users.noreply.github.com>
2024-10-05 23:11:12 -0300

    Update start_page.dart

--- lib/start_page.dart
+++ lib/start_page.dart
@@ -9,72 +9,167 @@ class StartPage extends StatefulWidget {
 }
 
 class StartPageState extends State<StartPage> {
-  final String _gameName = 'Logicon';
+  final String _gameName = 'Logisticon';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        // appBar: AppBar(
-        //   title: Text('Logicon'),
-        // ),  // Builder(builder: (BuildContext context) { return ... } )
-        body: Builder(builder: (BuildContext context) {
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                const SizedBox(height: 100),
-                Text(
-                  _gameName,
-                  style: const TextStyle(fontFamily: 'Oswald', fontSize: 60),
+        body: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/background5.png'),
+                  fit: BoxFit.cover,
                 ),
-                const SizedBox(height: 200),
-                //  a row with three buttons: one for starting the game,
-                //  one for showing the rules, and one for exiting the game
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    // a button with text 'Start game' that starts the game
-                    ElevatedButton(
-                      onPressed: () {
-                        // Navigator.pushNamed(context, '/game');
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => const LogicalApp()));
-                      },
-                      child: const Text('Start game'),
-                    ),
-                    const SizedBox(width: 20),
-                    // a button with text 'Show rules' that shows the rules
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => const ShowRules()));
-                      },
-                      child: const Text('Show rules'),
-                    ),
-                    const SizedBox(width: 20),
-                    // a button with text 'Exit game' that exits the game
-                    ElevatedButton(
-                      onPressed: () {
-                        // exit the App by popping up all routes
-                        Navigator.of(context)
-                            .popUntil((route) => route.isFirst);
-                        Navigator.of(context).pop();
-                      },
-                      child: const Text('Exit game'),
-                    ),
-                  ],
-                ),
-              ],
+              ),
             ),
-          );
-        }),
+            // Conteúdo principal
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  // Título do jogo
+                  Text(
+                    _gameName,
+                    style: const TextStyle(
+                      fontFamily: 'Roboto',
+                      fontSize: 80,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                      shadows: [
+                        Shadow(
+                          blurRadius: 10.0,
+                          color: Colors.black54,
+                          offset: Offset(6.0, 6.0),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 80),
+                  // Coluna com os botões
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      // Botão 'Start Game' com tamanho fixo
+                      SizedBox(
+                        width: 250, // Define uma largura fixa
+                        height: 60, // Define uma altura fixa
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                  builder: (context) => const LogicalApp()),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.white,
+                            backgroundColor: Colors.lightBlue,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                            ),
+                            shadowColor: Colors.black,
+                            elevation: 10,
+                          ),
+                          child: const Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.play_arrow_rounded,
+                                  color: Colors.white, size: 30),
+                              SizedBox(width: 15),
+                              Text('Start',
+                                  style: TextStyle(
+                                    fontSize: 28,
+                                    color: Colors.white,
+                                  )),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 40),
+                      // Botão 'Show Rules' com tamanho fixo
+                      SizedBox(
+                        width: 250, // Define uma largura fixa
+                        height: 60, // Define uma altura fixa
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                  builder: (context) => const ShowRules()),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.lightBlue,
+                            foregroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                            ),
+                            shadowColor: Colors.black,
+                            elevation: 10,
+                          ),
+                          child: const Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.rule_rounded,
+                                  color: Colors.white, size: 30),
+                              SizedBox(width: 15),
+                              Text('Rules',
+                                  style: TextStyle(
+                                      fontSize: 28, color: Colors.white)),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 40),
+                      // Botão 'Exit Game' com tamanho fixo
+                      SizedBox(
+                        width: 250, // Define uma largura fixa
+                        height: 60, // Define uma altura fixa
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context)
+                                .popUntil((route) => route.isFirst);
+                            Navigator.of(context).pop();
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.lightBlue,
+                            foregroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                            ),
+                            shadowColor: Colors.black,
+                            elevation: 10,
+                          ),
+                          child: const Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.exit_to_app_rounded,
+                                  color: Colors.white, size: 28),
+                              SizedBox(width: 15),
+                              Text('Exit',
+                                  style: TextStyle(
+                                      fontSize: 28, color: Colors.white)),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
-// create a stateful widget called ShowRules
 class ShowRules extends StatefulWidget {
   const ShowRules({super.key});
 
@@ -85,28 +180,97 @@ class ShowRules extends StatefulWidget {
 class ShowRulesState extends State<ShowRules> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Rules')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // a text widget with the rules of the game
-            const Text(
-                '''The rules of the game are simple. You will be presented with a 
-                  syllogism, and you will have to determine whether it is valid 
-                  or invalid. If you think it is valid, press the 'Valid' button. If 
-                  you think it is invalid, press the 'Invalid' button. If you want 
-                  an explanation of the syllogism, press the 'Explanation' button.'''),
-            const SizedBox(height: 20),
-            // a button with text 'Back' that goes back to the start page
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('Back'),
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/background4.png"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+            title: const Text('Rules',
+                style: TextStyle(
+                  fontSize: 32,
+                  color: Colors.white,
+                )),
+            backgroundColor: Colors.transparent,
+            iconTheme: const IconThemeData(color: Colors.white, size: 32)),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Center(
+            child: Container(
+              width: 1500,
+              height: 500,
+              decoration: BoxDecoration(
+                color:
+                    Colors.white.withOpacity(0.6), // fundo branco translúcido
+                borderRadius: BorderRadius.circular(10), // bordas arredondadas
+              ),
+              padding:
+                  const EdgeInsets.all(16.0), // padding interno para o texto
+              child: const Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Column(
+                    children: [
+                      Align(
+                        child: Text(
+                          'Game Rules',
+                          style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Column(
+                        children: [
+                          SizedBox(height: 10),
+                          Text(
+                            'The game is based on syllogisms, which are logical arguments that apply deductive reasoning to arrive at a conclusion based on two premises. A syllogism is valid if the conclusion follows logically from the premises.',
+                            style: TextStyle(
+                                fontSize: 32, fontWeight: FontWeight.w500),
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            'The game will present you with a syllogism, and you will have to determine which one is valid.',
+                            style: TextStyle(
+                                fontSize: 32, fontWeight: FontWeight.w500),
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            '• If you think it is valid, select the option and press the "Verify" button.',
+                            style: TextStyle(
+                                fontSize: 32, fontWeight: FontWeight.w500),
+                            textAlign: TextAlign.start,
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            '• If you want an explanation of the syllogism, press the "(?)" button.',
+                            style: TextStyle(
+                                fontSize: 32, fontWeight: FontWeight.w500),
+                            textAlign: TextAlign.start,
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            '• If you want to see your history, press the "Clock" button.',
+                            style: TextStyle(
+                                fontSize: 32, fontWeight: FontWeight.w500),
+                            textAlign: TextAlign.start,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ],
+          ),
         ),
       ),
     );
